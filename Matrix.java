aacc711136f1c9037a272e2c55a26e7f8aad0ae7
Deepraj-Patel <160591042+Deepraj-Patel@users.noreply.github.com>
2025-08-30 21:46:49 +0530

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,49 @@
+public class Matrix{
+
+    public int[][] coFactor(int[][] t,int r, int c){
+        int n = t.length;
+        int m = t[0].length;
+        int coFact[][] = new int[n-1][n-1];
+        int coI=0;
+        
+        for(int i=0;i<n;i++){
+            if(i == r){
+                continue;
+            }
+            int coJ=0;
+            for(int j=0;j<m;j++){
+                if(j == c){
+                    continue;
+                }
+                coFact[coI][coJ] = t[i][j];
+                coJ++;
+            }
+            coI++;
+        }
+        return coFact;
+    }
+    public void display(int[][] t){
+        int n =t.length;
+        int m = t[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(t[i][j]+" ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+    public static void main(String s[]){
+        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        
+        Matrix obj = new Matrix();
+        obj.display(matrix);
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                obj.display(obj.coFactor(matrix,i,j));
+            }
+        }
+    }
+}
